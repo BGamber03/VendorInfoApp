c05ebc4f195a607535a3ba25531b7ac97dfe7765
BGamber03 <bgamber03@ksu.edu>
2024-09-12 20:35:11 -0500

    Initial commit

---
+++ VendorInfoApp/VendorInfo.Designer.cs
@@ -0,0 +1,433 @@
+﻿namespace VendorInfoApp
+{
+    partial class VendorInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdHerbs = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstCity = new System.Windows.Forms.ListBox();
+            this.txtCell = new System.Windows.Forms.TextBox();
+            this.txtCompany = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rdCream = new System.Windows.Forms.RadioButton();
+            this.rdMilk = new System.Windows.Forms.RadioButton();
+            this.rdGreenTea = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rdCoffee = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.ckOntime = new System.Windows.Forms.CheckBox();
+            this.ckDelayed = new System.Windows.Forms.CheckBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.ckUnpaid = new System.Windows.Forms.CheckBox();
+            this.ckPaid = new System.Windows.Forms.CheckBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rdHerbs
+            // 
+            this.rdHerbs.AutoSize = true;
+            this.rdHerbs.Location = new System.Drawing.Point(88, 118);
+            this.rdHerbs.Name = "rdHerbs";
+            this.rdHerbs.Size = new System.Drawing.Size(65, 20);
+            this.rdHerbs.TabIndex = 1;
+            this.rdHerbs.TabStop = true;
+            this.rdHerbs.Text = "Herbs";
+            this.rdHerbs.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstCity);
+            this.groupBox1.Controls.Add(this.txtCell);
+            this.groupBox1.Controls.Add(this.txtCompany);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(23, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(330, 418);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Vendor Details";
+            // 
+            // lstCity
+            // 
+            this.lstCity.FormattingEnabled = true;
+            this.lstCity.ItemHeight = 16;
+            this.lstCity.Items.AddRange(new object[] {
+            "Abilene",
+            "Andover",
+            "Arkansas City",
+            "Atchison",
+            "Augusta",
+            "Baldwin City",
+            "Bel Aire",
+            "Bonner Springs",
+            "Chanute",
+            "Coffeyville",
+            "Colby",
+            "Concordia",
+            "De Soto",
+            "Derby",
+            "Dodge City",
+            "El Dorado",
+            "Emporia",
+            "Eudora",
+            "Fort Scott",
+            "Garden City",
+            "Gardner",
+            "Great Bend",
+            "Hays",
+            "Haysville",
+            "Hutchinson",
+            "Independence",
+            "Iola",
+            "Junction City",
+            "Kansas City",
+            "Lansing",
+            "Lawrence",
+            "Leavenworth",
+            "Leawood",
+            "Lenexa",
+            "Liberal",
+            "Manhattan",
+            "McPherson",
+            "Merriam",
+            "Mission",
+            "Newton",
+            "Olathe",
+            "Ottawa",
+            "Overland Park",
+            "Paola",
+            "Park City",
+            "Parsons",
+            "Pittsburg",
+            "Prairie Village",
+            "Pratt",
+            "Roeland Park",
+            "Salina",
+            "Shawnee",
+            "Spring Hill",
+            "Stilwell",
+            "Topeka",
+            "Ulysses",
+            "Valley Center",
+            "Wellington",
+            "Wichita",
+            "Winfield"});
+            this.lstCity.Location = new System.Drawing.Point(107, 137);
+            this.lstCity.Name = "lstCity";
+            this.lstCity.Size = new System.Drawing.Size(195, 244);
+            this.lstCity.TabIndex = 2;
+            // 
+            // txtCell
+            // 
+            this.txtCell.Location = new System.Drawing.Point(105, 96);
+            this.txtCell.Name = "txtCell";
+            this.txtCell.Size = new System.Drawing.Size(198, 22);
+            this.txtCell.TabIndex = 1;
+            // 
+            // txtCompany
+            // 
+            this.txtCompany.Location = new System.Drawing.Point(105, 68);
+            this.txtCompany.Name = "txtCompany";
+            this.txtCompany.Size = new System.Drawing.Size(198, 22);
+            this.txtCompany.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(105, 39);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(198, 22);
+            this.txtName.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "City";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Cell";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Company";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdCream);
+            this.groupBox2.Controls.Add(this.rdMilk);
+            this.groupBox2.Controls.Add(this.rdHerbs);
+            this.groupBox2.Controls.Add(this.rdGreenTea);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.rdCoffee);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(408, 20);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(330, 252);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Products Infortmation";
+            // 
+            // rdCream
+            // 
+            this.rdCream.AutoSize = true;
+            this.rdCream.Location = new System.Drawing.Point(88, 205);
+            this.rdCream.Name = "rdCream";
+            this.rdCream.Size = new System.Drawing.Size(68, 20);
+            this.rdCream.TabIndex = 1;
+            this.rdCream.TabStop = true;
+            this.rdCream.Text = "Cream";
+            this.rdCream.UseVisualStyleBackColor = true;
+            // 
+            // rdMilk
+            // 
+            this.rdMilk.AutoSize = true;
+            this.rdMilk.Location = new System.Drawing.Point(88, 179);
+            this.rdMilk.Name = "rdMilk";
+            this.rdMilk.Size = new System.Drawing.Size(52, 20);
+            this.rdMilk.TabIndex = 1;
+            this.rdMilk.TabStop = true;
+            this.rdMilk.Text = "Milk";
+            this.rdMilk.UseVisualStyleBackColor = true;
+            // 
+            // rdGreenTea
+            // 
+            this.rdGreenTea.AutoSize = true;
+            this.rdGreenTea.Location = new System.Drawing.Point(88, 92);
+            this.rdGreenTea.Name = "rdGreenTea";
+            this.rdGreenTea.Size = new System.Drawing.Size(93, 20);
+            this.rdGreenTea.TabIndex = 1;
+            this.rdGreenTea.TabStop = true;
+            this.rdGreenTea.Text = "Green Tea";
+            this.rdGreenTea.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 16);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Dairy Products";
+            // 
+            // rdCoffee
+            // 
+            this.rdCoffee.AutoSize = true;
+            this.rdCoffee.Location = new System.Drawing.Point(88, 66);
+            this.rdCoffee.Name = "rdCoffee";
+            this.rdCoffee.Size = new System.Drawing.Size(109, 20);
+            this.rdCoffee.TabIndex = 1;
+            this.rdCoffee.TabStop = true;
+            this.rdCoffee.Text = "Coffee Beans";
+            this.rdCoffee.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Products";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.ckDelayed);
+            this.groupBox3.Controls.Add(this.ckOntime);
+            this.groupBox3.Location = new System.Drawing.Point(408, 278);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(181, 160);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Delivery";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // ckOntime
+            // 
+            this.ckOntime.AutoSize = true;
+            this.ckOntime.Location = new System.Drawing.Point(47, 33);
+            this.ckOntime.Name = "ckOntime";
+            this.ckOntime.Size = new System.Drawing.Size(74, 20);
+            this.ckOntime.TabIndex = 0;
+            this.ckOntime.Text = "On time";
+            this.ckOntime.UseVisualStyleBackColor = true;
+            this.ckOntime.CheckedChanged += new System.EventHandler(this.ckOntime_CheckedChanged);
+            // 
+            // ckDelayed
+            // 
+            this.ckDelayed.AutoSize = true;
+            this.ckDelayed.Location = new System.Drawing.Point(47, 59);
+            this.ckDelayed.Name = "ckDelayed";
+            this.ckDelayed.Size = new System.Drawing.Size(81, 20);
+            this.ckDelayed.TabIndex = 0;
+            this.ckDelayed.Text = "Delayed";
+            this.ckDelayed.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.ckUnpaid);
+            this.groupBox4.Controls.Add(this.ckPaid);
+            this.groupBox4.Location = new System.Drawing.Point(595, 278);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(181, 160);
+            this.groupBox4.TabIndex = 0;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Payment";
+            this.groupBox4.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // ckUnpaid
+            // 
+            this.ckUnpaid.AutoSize = true;
+            this.ckUnpaid.Location = new System.Drawing.Point(47, 59);
+            this.ckUnpaid.Name = "ckUnpaid";
+            this.ckUnpaid.Size = new System.Drawing.Size(73, 20);
+            this.ckUnpaid.TabIndex = 0;
+            this.ckUnpaid.Text = "Unpaid";
+            this.ckUnpaid.UseVisualStyleBackColor = true;
+            // 
+            // ckPaid
+            // 
+            this.ckPaid.AutoSize = true;
+            this.ckPaid.Location = new System.Drawing.Point(47, 33);
+            this.ckPaid.Name = "ckPaid";
+            this.ckPaid.Size = new System.Drawing.Size(57, 20);
+            this.ckPaid.TabIndex = 0;
+            this.ckPaid.Text = "Paid";
+            this.ckPaid.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(657, 473);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(118, 31);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(658, 511);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(118, 31);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // VendorInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 554);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "VendorInfo";
+            this.Text = "Form2";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstCity;
+        private System.Windows.Forms.TextBox txtCell;
+        private System.Windows.Forms.TextBox txtCompany;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rdCoffee;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rdCream;
+        private System.Windows.Forms.RadioButton rdMilk;
+        private System.Windows.Forms.RadioButton rdGreenTea;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox ckDelayed;
+        private System.Windows.Forms.CheckBox ckOntime;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.CheckBox ckUnpaid;
+        private System.Windows.Forms.CheckBox ckPaid;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.RadioButton rdHerbs;
+    }
+}
---
+++ VendorInfoApp/VendorInfo.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VendorInfoApp
+{
+    public partial class VendorInfo : Form
+    {
+        public VendorInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // 1. Retrieve vendor details
+            string vendorName = txtName.Text;
+            string company = txtCompany.Text;
+            string phone = txtCell.Text;
+            string city = lstCity.GetItemText(lstCity.SelectedItem);
+
+            // 2. Determine selected product
+            string product = "";
+            if (rdCoffee.Checked)
+            {
+                product = "Coffee Beans";
+            }
+            else if (rdGreenTea.Checked)
+            {
+                product = "Green Tea";
+            }
+            else if (rdHerbs.Checked)
+            {
+                product = "Herbs";
+            }
+
+            // 3. Determine selected dairy product (Check for selection and avoid an empty string)
+            string dairyProduct = "";
+            if (rdMilk.Checked)
+            {
+                dairyProduct = "Milk";
+            }
+            else if (rdCream.Checked)
+            {
+                dairyProduct = "Cream";
+            }
+
+            // Optional: Handle cases where no dairy product is selected
+            if (string.IsNullOrEmpty(dairyProduct))
+            {
+                dairyProduct = "no dairy product";
+            }
+
+            // 4. Determine delivery status
+            string deliveryStatus = "";
+            if (ckOntime.Checked)
+            {
+                deliveryStatus = "on time";
+            }
+            else if (ckDelayed.Checked)
+            {
+                deliveryStatus = "delayed";
+            }
+
+            // 5. Determine payment status
+            string paymentStatus = "";
+            if (ckPaid.Checked)
+            {
+                paymentStatus = "paid";
+            }
+            else if (ckUnpaid.Checked)
+            {
+                paymentStatus = "unpaid";
+            }
+
+            // 6. Construct the final message to show in the MessageBox
+            string message = $"The vendor is {vendorName}, from {company}, they delivered {product}, {dairyProduct}, {deliveryStatus} and {paymentStatus}.";
+
+            // 7. Show the message in a MessageBox
+            MessageBox.Show(message, "Vendor Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Exit the application
+            Application.Exit();
+        }
+
+
+        private void ckOntime_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
